0280b3c8a5c069ad52debc9a4918234c9d0a8c2f
huajian <2876439672@qq.com>
2019-09-16 22:08:17 +0800

    添加新的banner

--- js/banner5.js
+++ js/banner5.js
@@ -7,8 +7,16 @@ var goPreBtn = document.getElementsByClassName("pre");
 // 下一张按钮对象
 var goNexBtn = document.getElementsByClassName("next");
 var index = 0;// index 表示第几张图片展示
-
+// 最下面一排小点对象
+var points = document.querySelectorAll(".dottes span");
 var timeOutFlag = null;
+
+// 移除下面小点的选中类active
+function cleanActive() {
+    for (var i = 0; i < points.length; i++) {
+        points[i].className = "";
+    }
+}
 
 // 点击下一张按钮
 goNexBtn[0].addEventListener('click', function () {
@@ -16,17 +24,12 @@ goNexBtn[0].addEventListener('click', function () {
         return
     }
     changeIndex("next");
-    var marginLeft = parseFloat(getElementStyle(bannerList[0], 'marginLeft'));
-    var width = parseFloat(getElementStyle(bannerItems[0], 'width'));
-    bannerList[0].style.marginLeft = marginLeft - width + 'px';
-    if (-width * bannerItems.length === parseFloat(bannerList[0].style.marginLeft)) {
-        bannerList[0].style.marginLeft = "0px"
-    }
+    goIndex();
+    resertInterval();
+    // 这是为了不让按钮在0.3秒内连续点击，不然会因动画延迟而产生轮播图错位
     timeOutFlag = setTimeout(function () {
         timeOutFlag = null
     }, 300)
-
-
 });
 
 // 点击上一张按钮
@@ -35,18 +38,20 @@ goPreBtn[0].addEventListener('click', function () {
         return
     }
     changeIndex("pre");
-    var marginLeft = parseFloat(getElementStyle(bannerList[0], 'marginLeft'));
-    var width = parseFloat(getElementStyle(bannerItems[0], 'width'));
-
-    if (marginLeft === 0) {
-        bannerList[0].style.marginLeft = -(width * (bannerItems.length - 1)) + "px";
-    } else {
-        bannerList[0].style.marginLeft = width + marginLeft + 'px';
-    }
+    goIndex();
+    resertInterval();
     timeOutFlag = setTimeout(function () {
         timeOutFlag = null
     }, 300)
 });
+
+for (var i = 0; i < points.length; i++) {
+    points[i].addEventListener("click", function () {
+        index = this.getAttribute("dot-index");
+        goIndex();
+        resertInterval();
+    })
+}
 
 changeIndex = function (type) {
     if (type === "pre") {
@@ -70,3 +75,24 @@ function getElementStyle(obj, attr) {
         return getComputedStyle(obj, false)[attr];   //第二个参数是伪类
     }
 }
+
+// 去当前被选中的那一个对象
+var goIndex = function () {
+    cleanActive();
+   // bannerItems[index].className = "banner-item active";
+    points[index].className = "active";
+    var width = parseFloat(getElementStyle(bannerItems[0], 'width'));
+    bannerList[0].style.marginLeft = -(width * (+index)) + "px";
+};
+
+var carouseFn = function () {
+    changeIndex("next");
+    goIndex()
+};
+
+var interval = setInterval(carouseFn, 3000);
+
+var resertInterval = function () {
+    clearInterval(interval);
+    interval = setInterval(carouseFn, 3000);
+};
