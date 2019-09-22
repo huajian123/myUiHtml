8ec10137774c339d760ae03db75e50a2071fda78
huajian <2876439672@qq.com>
2019-09-22 21:33:27 +0800

    添加新的banner

--- js/byton/index.js
+++ js/byton/index.js
@@ -1,7 +1,9 @@
 var adv = document.getElementsByClassName('adv')[0];
 window.onscroll = function () {
     var top = document.documentElement.scrollTop || document.body.scrollTop;
-    if (top > 990) {
+
+    // 这里的100是 transform: translateY(100px);
+    if (top > (adv.offsetTop - adv.offsetHeight - 100)) {
         adv.className = "adv play";
     } else {
         adv.className = "adv";
